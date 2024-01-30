6245983383370f37a1efd68b4f261da5a2d3a95d
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-01-31 02:31:58 +0700

    edit landing header

--- src/component/NavigationBar/LandingHeader.jsx
+++ src/component/NavigationBar/LandingHeader.jsx
@@ -1,21 +1,30 @@
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 import './LandingHeader.css'; // Assuming this is the filename for your CSS file
 
 export default function LandingHeader() {
     return (
         <>
             <nav className="landing-header">
-                <Link to="/"><h1>Motor monitoring</h1></Link>
+                <Link to="/"><img src="./logoV1.png" /></Link>
                 <ul>
+                    
                     <li>
-                        <Link to="/home">Home</Link>
+                        <Link to="/about-us">ABOUT</Link>
                     </li>
                     <li>
-                        <Link to="/about-us">About Us</Link>
+                        <Link to="/feature">FEATURE</Link>
                     </li>
                     <li>
-                        <Link to="/contact-us">Contact</Link>
+                        <Link to="/contact-us">CONTACT</Link>
                     </li>
+                    <li className="signin">
+                    <Link to="/sign-in">SIGN IN</Link>
+                    </li>
+                    <li>
+                    <FiMenu />
+                    </li>
+                    
                 </ul>
             </nav>
         </>
