a30342b3f27b425ea1691720474e1544f014a94b
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-02-04 01:34:48 +0700

    add image and other component into feature + add css

--- src/Page/Feature/Featurecomponent.jsx
+++ src/Page/Feature/Featurecomponent.jsx
@@ -1,13 +1,52 @@
-import { SiGmail } from "react-icons/si";
-import { FaYoutube } from "react-icons/fa";
-import { FaFacebookSquare } from "react-icons/fa";
-export default function Featurecomponent(){
-    return(
-        <><FaFacebookSquare />
-        <SiGmail />
-        
-        
-        </>
-    )
+import Feature_pic from "./Featurepic.png";
+import Health_monitor from "./image1.png";
+import Output_tracking from "./image2.png";
+import Downtime from "./image3.png";
+import Protection_data from "./image4.png";
+import Multiview from "./image5.png";
+import Sensor from "./image6.png";
+import "./Featurecomponent.css";
+export default function Featurecomponent() {
+  return (
+    <>
+      <div className="Feature-container">
+        <div className="feature-header">
+          <div className="anf-motor">ANF Motor Monitoring System Feature</div>
+          <img className="Feature-pic" src={Feature_pic} />
+        </div>
 
+        <div className="Feature-list">
+          <div className="Health-monitoring">
+            <img className="Health-monitoring-img" src={Health_monitor} />
+            <p>Machine Health Monitoring</p>
+          </div>
+
+          <div className="Output-tracking">
+            <img className="Output-tracking-img" src={Output_tracking} />
+            <p>Production Output Tracking</p>
+          </div>
+
+        <div className="Downtime">
+        <img className="Downtime-img" src={Downtime} />
+            <p>Downtime Notification</p>
+        </div>
+
+          <div className="Protection-data">
+            <img className="Protection-data-img" src={Protection_data} />
+            <p>Production Data</p>
+          </div>
+
+          <div className="Multiview">
+            <img className="Multiview-img" src={Multiview} />
+            <p>Multiview Mornitoring</p>
+          </div>
+
+          <div className="Sensor">
+            <img className="Sensor-img" src={Sensor} />
+            <p>Sensors on machines</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
