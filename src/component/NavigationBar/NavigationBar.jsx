30e77116f6719ced009a58b593e426bb941c95c2
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-03-02 15:42:42 +0700

    style (navigation bar): change the css in main navigation bar

--- src/component/NavigationBar/NavigationBar.jsx
+++ src/component/NavigationBar/NavigationBar.jsx
@@ -1,13 +1,19 @@
 import { GoSignOut } from "react-icons/go";
 import { Link } from "react-router-dom"
+import logo from "./logoV1.png"
 import './NavigationBar.css'
 export default function NavigationBar(){
     return(<>
     <nav className="NavNavigate">
         <ul className="ListNavigate">
+            <li>
+            <img src={logo} alt="logo" />
+            </li>
+            <div className="dropdown">
             <li> 
             <Link to="/all-motors">ALL MOTORS</Link>
             </li>
+            </div>
             <li>
             <Link to="/favorite">FAV motor</Link>
             </li>
