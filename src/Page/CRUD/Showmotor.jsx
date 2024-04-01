6e34b0a842185bf12552fc0efc4b527d3c5727ef
Artit1515 <arthit.lue@dome.tu.ac.th>
2024-04-02 05:59:05 +0700

    change the mockup db and change the showmotor logic

--- src/Page/CRUD/Showmotor.jsx
+++ src/Page/CRUD/Showmotor.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 const ShowMotor = () => {
   const allMotorsApi = "http://localhost:3000/motors";
-
   const [motors, setMotors] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -18,6 +17,7 @@ const ShowMotor = () => {
     axios
       .get(allMotorsApi)
       .then((res) => {
+        console.log("API Response:", res.data);
         setMotors(res.data);
         setIsLoading(false);
       })
@@ -39,11 +39,24 @@ const ShowMotor = () => {
   return (
     <div>
       <h1>All Motors</h1>
-      {motors.map((motor) => (
-        <div key={motor.motor_id}>
-          <Link to={`/show-motor/${motor.motor_id}`}>{motor.motor_id}</Link>
-        </div>
-      ))}
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Motor ID</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {motors.map((motor) => (
+            <tr key={motor._id}>
+              <td>{motor.motor_id}</td>
+              <td>
+                <Link to={`/show-motor/${motor.motor_id}`}>View Details</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
